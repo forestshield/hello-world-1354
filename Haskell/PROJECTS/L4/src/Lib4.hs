4d5472eeef74d1cc36dda773daba10faa1966ba5
forestshield <xpath@usa.com>
2020-07-24 22:41:49 -0400

    Add files via upload

--- Haskell/PROJECTS/L4/src/Lib4.hs
+++ Haskell/PROJECTS/L4/src/Lib4.hs
@@ -23,10 +23,23 @@ someFuncLib4 = do
   putStrLn ".......myAbs2 5 ..............." 
   print (myAbs2 5)
   putStrLn ".......simpliest func with Lambda syntaxis .........." 
-  print $ id3 "xxx"
-  print $ id3 5
-  print $ id2 [1,2,4]
-  print $ id2 (5, "tuples")
+  putStrLn "id :: a -> a    -- type singnature"
+  putStrLn "id x = x        -- regular definition"
+  putStrLn "id = \\x -> x    -- lambda syntaxis definition"  -- prints id = \x -> x
+  print $ id3 "xxx"                                 -- "xxx"
+  print $ id3 5                                     -- 5  
+  print $ id2 [1,2,4]                               -- [1,2,4]
+  print $ id2 (5, "tuples")                         -- (5, "tuples")
+  putStrLn "....... applyTwice .........." 
+  print $ applyTwice id3 11                   -- 11
+  print $ applyTwice add1 11                  -- 13
+  print $ applyTwice reverse (reverse "abc")  -- "cba"
+  print $ add 6 7                             -- 13
+  putStrLn "....... applyThree .........." 
+  print $ applyThree id3 11                   -- 11
+  print $ applyThree add1 11                  -- 14
+  print $ applyThree reverse "abc"            -- "cba"
+  
 
 
 --working with Lists
@@ -63,13 +76,26 @@ myAbs2 x = do
   else 
     negate x
 
---simpliest function
+--simpliest function, regular definition
 id3 :: a -> a
 id3 x = x
 
---simpliest function, lambda notation
+--simpliest function, lambda sysntaxis
 id2 :: a -> a
 id2  = \x -> x
 
---
+-- applyTwice 
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
 
+-- add
+add :: (Num a) => a -> a -> a 
+add x y = x + y
+
+-- add1
+add1 :: Int -> Int
+add1 x = x + (1 :: Int)
+
+-- applyThree
+applyThree :: (a -> a) -> a -> a
+applyThree f x = f (f (f x))
