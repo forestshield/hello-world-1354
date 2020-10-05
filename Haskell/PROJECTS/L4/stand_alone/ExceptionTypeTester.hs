2a0836ef89e09b7c83e95bae6cafa0f473eedd16
forestshield <xpath@usa.com>
2020-10-05 13:07:50 -0400

    Add files via upload

---
+++ Haskell/PROJECTS/L4/stand_alone/ExceptionTypeTester.hs
@@ -0,0 +1,31 @@
+
+import Control.Exception
+import Data.Typeable (typeOf)
+
+testExceptionType :: IO () -> IO ()
+testExceptionType thunk =  catch thunk handler
+  where
+    -- Catch All Exceptions -- It is not recommended in real life.
+    handler :: SomeException -> IO ()
+    handler (SomeException e) = putStrLn $ "I caught an exception.\nMessage =  \
+    \" ++ show e ++ "\nType of exception = " ++ show (typeOf e)
+
+--divByZero      =  
+--fatalKernelErr = 
+
+main :: IO ()
+main = do
+  testExceptionType (print $ div 10 0)
+  print "---"
+  testExceptionType (error "Fatal kernel error")
+  print "---"
+  testExceptionType (readFile "/etc/shadow" >>= putStrLn)
+  print "---"
+  testExceptionType (print $ head ([] :: [Int]))
+  print "---"
+  --testExceptionType (print $ head [])
+  --testExceptionType (head [])
+  testExceptionType (print $ head [1, 2, 3])
+  print "---"
+  --testExceptionType (putStrLn "Insert a line" >> getLine >>= putStrLn)
+  
